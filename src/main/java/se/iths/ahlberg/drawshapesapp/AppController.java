e47a8669b7d95f34bb5154f9663972639fb25a1e
undefined <undefined>
2022-12-10 21:36:15 +0100

    Create controller fields connected to FX elements

--- src/main/java/se/iths/ahlberg/drawshapesapp/AppController.java
+++ src/main/java/se/iths/ahlberg/drawshapesapp/AppController.java
@@ -1,9 +1,19 @@
 package se.iths.ahlberg.drawshapesapp;
 
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.*;
 
 public class AppController {
 
-
-
+    public Canvas canvas;
+    public Button undoButton;
+    public Button redoButton;
+    public Button connectButton;
+    public Button saveButton;
+    public Button updateButton;
+    public ToggleButton selectModeButton;
+    public ComboBox<Object> shapeComboBox; //TODO:: set <HERE>
+    public ColorPicker colorPicker;
+    public Slider sizeSlider;
 }
