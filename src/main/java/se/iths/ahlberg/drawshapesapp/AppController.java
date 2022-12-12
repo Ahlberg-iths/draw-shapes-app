ed4f6bb6b63c8a4add9b616022c6a0a7241fd7d5
undefined <undefined>
2022-12-12 06:59:34 +0100

    Add handleCanvasClicked with basic functionality

--- src/main/java/se/iths/ahlberg/drawshapesapp/AppController.java
+++ src/main/java/se/iths/ahlberg/drawshapesapp/AppController.java
@@ -1,7 +1,10 @@
 package se.iths.ahlberg.drawshapesapp;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class AppController {
 
@@ -24,5 +27,31 @@ public class AppController {
         shapeComboBox.valueProperty().bindBidirectional(model.shapeChoiceProperty());
         shapeComboBox.setItems(model.getShapeChoiceList());
         selectModeButton.selectedProperty().bindBidirectional(model.inSelectModeProperty());
+    }
+
+    public void handleCanvasClicked(MouseEvent mouseEvent) {
+
+        CanvasCoordinates coordinates = new CanvasCoordinates(mouseEvent.getX(), mouseEvent.getY());
+
+        if (model.isInSelectMode()) {
+            //TODO:: check which shape is clicked
+            //TODO:: put shape in some list with selected shapes ?
+        } else {
+            //TODO:: clear some list with selected shapes ? or do this on "in select mode button" click (off) instead?
+
+            ShapeChoice shapeChoice = model.getShapeChoice();
+            Number size = model.getSize();
+            Color color = model.getColor();
+
+            Shape newShape = Shape.of(shapeChoice, (Double)size, color, coordinates);
+
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+
+            newShape.draw(gc);
+
+            //TODO:: save shape to "shapes to draw" observable list - move drawing to event handler function
+            //TODO:: save shape to undo list
+
+        }
     }
 }
